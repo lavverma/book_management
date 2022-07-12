e923e515651b2913cb772223f5d6ba157c9ce15c
lavverma <lavverma109@gmail.com>
2022-07-12 23:56:15 +0530

    Final

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -92,7 +92,9 @@ const getBookByQuery = async function (req, res) {
         return res.status(200).send({ status: true, message: "Success", data: sortedBook })
       }
     } else {
-      let bookData = await bookModel.find({ $and: [fil, { isDeleted: false }] }).select({ title: 1, excerpt: 1, userId: 1, category: 1, reviews: 1, releasedAt: 1 })
+      if(fil.userId || fil.category || fil.subcategory){
+      if(fil){
+      let bookData = await bookModel.find({ $and:[fil, { isDeleted: false }] }).select({ title: 1, excerpt: 1, userId: 1, category: 1, reviews: 1, releasedAt: 1 })
       if (bookData.length == 0) {
         return res.status(404).send({ status: false, message: "Book is not present of this query!" })
       } else {
@@ -102,6 +104,12 @@ const getBookByQuery = async function (req, res) {
         return res.status(200).send({ status: true, message: "Success", data: sortedBook })
       }
     }
+    }else{
+      return res.status(400).send({ status: false, message: "query is invalid" })
+    }
+  
+}
+  
   }
   catch (err) {
     return res.status(500).send({ status: false, message: err.message });
